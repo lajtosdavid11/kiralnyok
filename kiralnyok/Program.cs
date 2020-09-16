c6ee77206f60de30be52ad855e069748cb756aed
lajtosdavid11 <lajtos.david11@gmail.com>
2020-09-16 08:29:05 +0200

    Add project files.

---
+++ kiralnyok/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kiralnyok
+{
+    class Tabla
+    {
+        private char UresCella;
+        private char UresOszlopSzam;
+        private char UresSorokSzama;
+
+        private char[,] matrix = new char[8,8 ];
+
+    }
+
+    public void elhelyezes()
+    {
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tabla t = new Tabla();
+            e
+
+            Console.ReadKey();
+        }
+    }
+}
